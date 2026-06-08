fb2491e46d8d6044b491773cad69df8fcf67d037
nschool102 <34006773+nschool102@users.noreply.github.com>
2026-06-08 16:15:14 +0700

    bản chạy offline

--- sw.js
+++ sw.js
@@ -1,39 +1,64 @@
-const CACHE_NAME = "app-v1";
+const CACHE_NAME = "app-cache-v1";
 
 const FILES = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./build.txt"
 ];
 
 self.addEventListener("install", event => {
+
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(FILES))
   );
 
   self.skipWaiting();
+
 });
 
 self.addEventListener("activate", event => {
+
   event.waitUntil(
+
     caches.keys().then(keys =>
       Promise.all(
         keys.map(key => {
+
           if (key !== CACHE_NAME) {
             return caches.delete(key);
           }
+
         })
       )
     )
+
   );
 
   self.clients.claim();
+
 });
 
 self.addEventListener("fetch", event => {
+
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+
+    fetch(event.request)
+
+      .then(response => {
+
+        const clone = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(cache => cache.put(event.request, clone));
+
+        return response;
+
+      })
+
+      .catch(() => caches.match(event.request))
+
   );
+
 });
